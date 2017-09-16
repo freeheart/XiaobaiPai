6582dc9c39e84d691ea0282cfce6afc01bf626ef
Jinzhu.Zhang <freeheart1977z@sina.com>
2017-09-16 23:37:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
 var https = require('https');
+
 var fs = require('fs');
 var basedir = '/var/webkey/';
 
 var options = {
-	key: fs.readFileSync(basedir + 'server-key.pem'),
-	ca: [fs.readFileSync(basedir + 'ca-cert.pem')],
-	cert: fs.readFileSync(basedir + 'server-cert.pem')
+	key: fs.readFileSync(basedir + 'server-key.pem','utf8'),
+	ca: [fs.readFileSync(basedir + 'ca-cert.pem','utf8')],
+	cert: fs.readFileSync(basedir + 'server-cert.pem','utf8')
 };
+
 //登录拦截器
 app.use(function (req, res, next) {
     var url = req.originalUrl;
@@ -15,8 +20,10 @@ app.use(function (req, res, next) {
     }
     next();
 });
+// 处理区域
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'POST' });
+});
+
 //启动HTTPS服务
-https.createServer(options,function(req,res){
-	res.writeHead(200);
-	res.end('hello world\n');
-}).listen(433,'127.0.0.1');
+https.createServer(options, app).listen(443);

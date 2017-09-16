8ffbee23501daedda7a2321a6b239a304b22173f
Jinzhu.Zhang <freeheart1977z@sina.com>
2017-09-16 22:59:25 +0800

    Update client_wxapp.js

--- client_wxapp.js
+++ client_wxapp.js
@@ -103,3 +103,55 @@ App({
     }   
    },  
 })  
+
+//获取openid方法二
+//页面加载 微信授权
+var getInfo = function (thisObj){
+  var that = thisObj;
+  wx.login({
+    success: function (res) {
+      if (res.code) {
+        //获取openId
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data: {
+　　　　　　　//小程序唯一标识
+            appid: '',
+　　 　　 　　//小程序的 app secret
+            secret: '',
+            grant_type: 'authorization_code',
+            js_code: res.code
+          },
+          method: 'GET',
+          header: { 'content-type': 'application/json'},
+          success: function(openIdRes){
+              console.info("登录成功返回的openId：" + openIdRes.data.openid);
+              weChatUserInfo.openId = openIdRes.data.openid;
+              // 判断openId是否获取成功
+              if (openIdRes.data.openid != null & openIdRes.data.openid != undefined) {
+　　　　　　　　// 有一点需要注意 询问用户 是否授权 那提示 是这API发出的
+                wx.getUserInfo({
+                  success: function (data) {
+                    // 自定义操作
+                    // 绑定数据，渲染页面
+                    that.setData({
+    
+                    });
+                  },
+                  fail: function (failData) {
+                      console.info("用户拒绝授权");
+                  }
+                });
+              }else {
+                 console.info("获取用户openId失败");
+              }
+          },
+          fail: function(error) {
+            console.info("获取用户openId失败");
+            console.info(error);
+          }
+        })
+      }
+    }
+  });
+} 

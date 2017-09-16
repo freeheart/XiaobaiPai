7230d840b4d6c65fe1fd485a45b48bbf61759938
Jinzhu.Zhang <freeheart1977z@sina.com>
2017-09-16 22:33:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ var options = {
 	ca: [fs.readFileSync(basedir + 'ca-cert.pem')],
 	cert: fs.readFileSync(basedir + 'server-cert.pem')
 };
-
+//登录拦截器
+app.use(function (req, res, next) {
+    var url = req.originalUrl;
+    if (url != "/login" && !req.session.user) {
+        return res.redirect("/login");
+    }
+    next();
+});
+//启动HTTPS服务
 https.createServer(options,function(req,res){
 	res.writeHead(200);
 	res.end('hello world\n');
